167c6b5a22e6ffc095dcc566cb2346715e6875f1
spym <spym200@qq.com>
2024-01-17 15:43:42 +0800

    admin 新增 login api

---
+++ routes/admin/UserRouter.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const UserController = require("../../controllers/admin/UserController");
+
+const UserRouter = express.Router();
+
+UserRouter.post("/adminapi/user/login", UserController.login);
+
+// UserRouter.get("/adminapi/user/home", (req, res) => {
+//   res.send({ ok: 1 });
+// });
+
+module.exports = UserRouter;
