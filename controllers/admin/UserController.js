28696d54128072fc806b27030b1d256081353ee9
spym <spym200@qq.com>
2024-01-18 11:18:11 +0800

    登录成功后返回用户信息

--- controllers/admin/UserController.js
+++ controllers/admin/UserController.js
@@ -19,7 +19,16 @@ const UserController = {
         "10s"
       );
       res.header("Authorization", token);
-      res.send({ ActionType: "OK" });
+      res.send({
+        ActionType: "OK",
+        data: {
+          username: result[0].username,
+          gender: result[0].gender ? result[0].gender : 0,
+          introduction: result[0].introduction,
+          avatar: result[0].avatar,
+          role: result[0].role,
+        },
+      });
     }
   },
 };
--- routes/admin/UserRouter.js
+++ routes/admin/UserRouter.js
@@ -5,8 +5,4 @@ const UserRouter = express.Router();
 
 UserRouter.post("/adminapi/user/login", UserController.login);
 
-UserRouter.get("/adminapi/user/home", (req, res) => {
-  res.send({ ok: 1 });
-});
-
 module.exports = UserRouter;
